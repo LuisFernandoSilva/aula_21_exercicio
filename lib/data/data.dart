bd5c6af730147819464dfe7a5f2f5f2b6049c5f7
LuisFernandoSilva <37556159+LuisFernandoSilva@users.noreply.github.com>
2020-07-18 11:37:54 -0300

    refatorado

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -1,3 +1,7 @@
 import 'package:aula_21_fllutter_exercicio/entites/user.dart';
 
-final registerList = <User>[];
+final registerList = <User>[
+  User.newer('luis', 38, 'l.@g.com.br');
+  User.newer('tiago', 25, 't.@g.com.br');
+  User.newer('denner', 22, 'd.@g.com.br');
+];
--- lib/pages/list_page.dart
+++ lib/pages/list_page.dart
@@ -9,43 +9,28 @@ class ListRegister extends StatefulWidget {
 
 class _ListRegisterState extends State<ListRegister> {
   var users = registerList;
-  var user1 = User.newer('luis', 38, 'l.@g.com.br');
-  var user2 = User.newer('tiago', 25, 't.@g.com.br');
-  var user3 = User.newer('denner', 22, 'd.@g.com.br');
 
   @override
   Widget build(BuildContext context) {
-    users.add(user1);
-    users.add(user2);
-    users.add(user3);
     return Scaffold(
         resizeToAvoidBottomInset: false,
         appBar: AppBar(
           title: Text('Lista de Cadastros'),
           centerTitle: true,
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: <Widget>[
-              Expanded(
-                child: ListView(
-                  children: users.map((user) {
-                    return ListTile(
-                      leading: Icon(Icons.person),
-                      title: Text('${user.name}, ${user.age}'),
-                      subtitle: Text(user.email),
-                      onLongPress: () {
-                        setState(() {
-                          users.remove(user);
-                        });
-                      },
-                    );
-                  }).toList(),
-                ),
-              ),
-            ],
-          ),
+        body: ListView(
+          children: users.map((user) {
+            return ListTile(
+              leading: Icon(Icons.person),
+              title: Text('${user.name}, ${user.age}'),
+              subtitle: Text(user.email),
+              onLongPress: () {
+                setState(() {
+                  users.remove(user);
+                });
+              },
+            );
+          }).toList(),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () async {
